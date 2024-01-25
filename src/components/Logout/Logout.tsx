020592b533518001fccdf321497103ade3098f62
naveen.singh <naveen.singh@itelasoft.com>
2024-01-25 13:40:58 +0530

    change logout logic

--- src/components/Logout/Logout.tsx
+++ src/components/Logout/Logout.tsx
@@ -6,29 +6,34 @@ const Logout: React.FC = () => {
   const navigate = useNavigate();
   const { logout } = useUser();
 
-  const handleLogout = async () => {
-    try {
-      const token = localStorage.getItem('authToken');
-      const response = await fetch('http://localhost:4000/logout', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
-        },
-      });
+  // const handleLogout = async () => {
+  //   try {
+  //     const token = localStorage.getItem('authToken');
+  //     const response = await fetch('http://localhost:4000/logout', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         'Authorization': `Bearer ${token}`,
+  //       },
+  //     });
 
-      if (response.ok) {
-        logout();
-        navigate('/login');
-      } else {
-        // Handle error cases
-        console.error('Logout failed:', response.statusText);
-      }
-    } catch (error) {
-      console.error('Logout error:', error);
-    }
-  };
+  //     if (response.ok) {
 
+  //       logout();
+  //       navigate('/login');
+  //     } else {
+  //       // Handle error cases
+  //       console.error('Logout failed:', response.statusText);
+  //     }
+  //   } catch (error) {
+  //     console.error('Logout error:', error);
+  //   }
+  // };
+
+  const handleLogout=()=>{
+    logout();
+    navigate('/login');
+  }
   return (
     <div>
       <button onClick={handleLogout}>Logout</button>
