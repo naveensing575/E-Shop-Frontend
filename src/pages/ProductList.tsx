f642c72546eeb657c4efcbd2b9e6ddca6794562b
naveen.singh <naveen.singh@itelasoft.com>
2024-02-05 14:34:07 +0530

    Add pagination in productList

--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Container, Row, Col, Card, Image, Alert } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col, Card, Image, Alert, Pagination } from 'react-bootstrap';
 import { useNavigate, useLocation } from 'react-router-dom';
 import axios from 'axios';
 import NoProductImage from '../assets/default.png';
@@ -22,52 +22,57 @@ const ProductList: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(true);
   const [sortCriteria, setSortCriteria] = useState<string>('priceAsc');
   const [error, setError] = useState<string>('');
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [productsPerPage] = useState<number>(8); // Number of products per page
   const navigate = useNavigate();
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const searchQuery = queryParams.get('search') ?? '';
 
   const fetchProducts = async () => {
-  try {
-    const userInfo = localStorage.getItem('userInfo');
-    const authToken = userInfo ? JSON.parse(userInfo).token : null;
-    const response = await axios.get(`http://localhost:4000/products`, {
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      },
-    });
+    try {
+      const userInfo = localStorage.getItem('userInfo');
+      const authToken = userInfo ? JSON.parse(userInfo).token : null;
+      const response = await axios.get(`http://localhost:4000/products`, {
+        params: {
+          page: currentPage, // Send the current page number as a parameter
+          limit: productsPerPage, // Send the number of products per page as a parameter
+        },
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
 
-    if (response.status === 200) {
-      setProducts(response.data);
-    } else {
-      console.error('Failed to fetch products:', response.statusText);
+      if (response.status === 200) {
+        setProducts(response.data);
+      } else {
+        console.error('Failed to fetch products:', response.statusText);
+      }
+    } catch (error: any) {
+      if (error.response && error.response.status === 403) {
+        handleForbiddenError(error);
+      } else {
+        console.error('Error fetching products:', error.response?.data?.error || error.message);
+      }
+    } finally {
+      setLoading(false);
     }
-  } catch (error: any) {
-    if (error.response && error.response.status === 403) {
-      handleForbiddenError(error);
+  };
+
+  const handleForbiddenError = (error: any) => {
+    const errorMessage = error.response.data?.error;
+    if (errorMessage === 'Forbidden - Invalid Token') {
+      // Show alert and redirect to login page
+      setError('You are not authorized. Please login again.');
+      navigate('/login');
     } else {
-      console.error('Error fetching products:', error.response?.data?.error || error.message);
+      console.error('Error fetching products:', errorMessage);
     }
-  } finally {
-    setLoading(false);
-  }
-};
+  };
 
-const handleForbiddenError = (error: any) => {
-  const errorMessage = error.response.data?.error;
-  if (errorMessage === 'Forbidden - Invalid Token') {
-    // Show alert and redirect to login page
-    setError('You are not authorized. Please login again.');
-    navigate('/login');
-  } else {
-    console.error('Error fetching products:', errorMessage);
-  }
-};
-
-useEffect(() => {
-  fetchProducts();
-}, []);
-
+  useEffect(() => {
+    fetchProducts();
+  }, [currentPage]); // Fetch products when the current page changes
 
   useEffect(() => {
     const filterAndSortProducts = () => {
@@ -115,62 +120,84 @@ useEffect(() => {
   };
 
   const truncateDescription = (description: string, maxLength: number = 25) => {
-  if (description.length > maxLength) {
-    return `${description.substring(0, maxLength)}...`;
-  }
-  return description;
-};
+    if (description.length > maxLength) {
+      return `${description.substring(0, maxLength)}...`;
+    }
+    return description;
+  };
 
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = filteredProducts.slice(indexOfFirstProduct, indexOfLastProduct);
+
+  const totalPages = Math.ceil(filteredProducts.length / productsPerPage);
+
+  const renderPagination = () => {
+    return (
+      <Pagination>
+        {Array.from({ length: totalPages }, (_, i) => (
+          <Pagination.Item key={i + 1} active={i + 1 === currentPage} onClick={() => handlePageChange(i + 1)}>
+            {i + 1}
+          </Pagination.Item>
+        ))}
+      </Pagination>
+    );
+  };
 
   const renderContent = () => {
-  if (loading) {
-    return <Loader />;
-  } 
-  if (error) {
-    return <Alert variant="danger">{error}</Alert>;
-  }
-  if (filteredProducts.length === 0) {
-    return <Alert variant="info">No products found for "{searchQuery}"</Alert>;
-  }
-  return (
-    <Row xs={1} md={2} lg={3} xl={4}>
-      {filteredProducts.map((product) => (
-        <Col key={product.productId} className="mb-4">
-          <Card className="product-card">
-            <Image
-              src={product?.image || NoProductImage}
-              alt="Product"
-              className="card-image"
-              onClick={() => handleProductDetails(product.productId)}
-            />
-            <Card.Body className="card-body" onClick={() => handleProductDetails(product.productId)}>
-              <Card.Title>{product.productName}</Card.Title>
-              <Card.Text className="mb-3">{truncateDescription(product.productDescription)}</Card.Text>
-              <Card.Text>Price: ${product.price}</Card.Text>
-            </Card.Body>
-            <Card.Footer className="d-flex justify-content-center">
-              <CartButton productId={product.productId} initialQuantity={0}/>
-            </Card.Footer>
-          </Card>
-        </Col>
-      ))}
-    </Row>
-  );
-};
+    if (loading) {
+      return <Loader />;
+    }
+    if (error) {
+      return <Alert variant="danger">{error}</Alert>;
+    }
+    if (currentProducts.length === 0) {
+      return <Alert variant="info">No products found for "{searchQuery}"</Alert>;
+    }
+    return (
+      <Row xs={1} md={2} lg={3} xl={4}>
+        {currentProducts.map((product) => (
+          <Col key={product.productId} className="mb-4">
+            <Card className="product-card">
+              <Image
+                src={product?.image || NoProductImage}
+                alt="Product"
+                className="card-image"
+                onClick={() => handleProductDetails(product.productId)}
+              />
+              <Card.Body className="card-body" onClick={() => handleProductDetails(product.productId)}>
+                <Card.Title>{product.productName}</Card.Title>
+                <Card.Text className="mb-3">{truncateDescription(product.productDescription)}</Card.Text>
+                <Card.Text>Price: ${product.price}</Card.Text>
+              </Card.Body>
+              <Card.Footer className="d-flex justify-content-center">
+                <CartButton productId={product.productId} initialQuantity={0} />
+              </Card.Footer>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    );
+  };
 
-return (
-  <Container>
-    <Row className="justify-content-between align-items-center">
-      <Col xs={12} md={6}>
-        <h2 className="mt-3 font">Today's Deals</h2>
-      </Col>
-      <Col xs={12} md={6} className="text-md-right mt-3 mt-md-0">
-        <SortProducts onChange={handleSortChange} />
-      </Col>
-    </Row>
-    {renderContent()}
-  </Container>
-);
+  return (
+    <Container>
+      <Row className="justify-content-between align-items-center">
+        <Col xs={12} md={6}>
+          <h2 className="mt-3 font">Today's Deals</h2>
+        </Col>
+        <Col xs={12} md={6} className="text-md-right mt-3 mt-md-0">
+          <SortProducts onChange={handleSortChange} />
+        </Col>
+      </Row>
+      {renderContent()}
+      {renderPagination()}
+    </Container>
+  );
 };
 
 export default ProductList;
