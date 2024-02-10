e38613dce8470a4bd6d8dea9887c048ce48f4be0
naveen.singh <naveen.singh@itelasoft.com>
2024-02-10 16:46:59 +0530

    minor update

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -2,20 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import axios from 'axios';
 
-interface Category {
-  categoryId: number;
-  categoryName: string;
-}
-
 const Categories: React.FC = () => {
-  const [categories, setCategories] = useState<Category[]>([]);
+  const [categories, setCategories] = useState<string[]>([]);
 
   useEffect(() => {
     const fetchCategories = async () => {
       try {
         const userInfo = localStorage.getItem('userInfo');
         const authToken = userInfo ? JSON.parse(userInfo).token : null;
-        const response = await axios.get<Category[]>('http://localhost:4000/products/categories', {
+        const response = await axios.get<string[]>('http://localhost:4000/products/categories', {
           headers: {
             Authorization: `Bearer ${authToken}`,
           },
@@ -33,8 +28,8 @@ const Categories: React.FC = () => {
     <Container>
       <h2 className="mt-3">Categories</h2>
       <ul>
-        {categories.map(category => (
-          <li key={category.categoryId}>{category.categoryName}</li>
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
         ))}
       </ul>
     </Container>
